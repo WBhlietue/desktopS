a058268fb8ccfbe2753a847c54b13a535425e217
Hakuran <WBhlietue@gmail.com>
2023-06-19 23:59:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const { BrowserWindow, app, screen } = require("electron");
+const CreateWindow = () => {
+    const win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        
+        // frame:false
+    });
+    win.maximize();
+    win.loadFile("./src/main/index.html");
+};
+app.whenReady().then(CreateWindow);
