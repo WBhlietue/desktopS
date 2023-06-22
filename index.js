4bb03378e2ecde93b16e6d18e71a5b4ca0c6ebb2
Hakuran <WBhlietue@gmail.com>
2023-06-22 23:14:57 +0800

    terminal

--- index.js
+++ index.js
@@ -17,19 +17,17 @@ const CreateWindow = () => {
   var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
   var ptyProcess = pty.spawn(shell, [], {
     name: "xterm-color",
-    cols: 80,
-    rows: 24,
     cwd: process.env.HOME,
-    env: process.env,
+    cols: 80,
+    rows: 30,
   });
+  
 
   ptyProcess.on("data", (data) => {
-    console.log("senddata");
     win.webContents.send("terminal-incData", data);
   });
 
   ipcMain.on("terminal-into", (event, data) => {
-    console.log(data+"_");
     ptyProcess.write(data);
   });
   win.maximize();
